5941d9355445651854c62ad7012275297f42031a
tora29 <nikumann.power@gmail.com>
2025-02-28 20:33:19 +0900

    S3 バケットのパブリックアクセスブロックを無効化する

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,15 @@ resource "aws_s3_bucket" "site_bucket" {
 }
 
 # S3 バケットポリシー
+resource "aws_s3_bucket_public_access_block" "site_bucket_public_access" {
+  bucket = aws_s3_bucket.site_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "site_bucket_policy" {
   bucket = aws_s3_bucket.site_bucket.id
   policy = jsonencode({
